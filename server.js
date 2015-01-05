71d22fbc0f64dedeef764b084caeb8821922316e
Damian Beresford <dberesford@gmail.com>
2015-01-05 15:16:10 +0000

    First cut

---
+++ server.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+var http    = require('http');
+var redis   = require('redis');
+var concat  = require('concat-stream');
+var client  = redis.createClient(6379, process.env.DOCKER_HOST_IP ||
+'localhost');
+var server  = http.createServer();
+var errors  = 0;
+
+client.on('error', function(err) {
+  console.log(err);
+  if (++errors === 10) {
+    process.exit(1);
+  }
+});
+
+server.on('request', function(req, res) {
+  if (req.method === 'GET') {
+    client.get(req.path, function(err, value) {
+      if (err) {
+        res.statusCode = 500;
+        res.end(err.message);
+        return;
+      }
+      res.end(value);
+    });
+  } else {
+    req.pipe(concat(function(list) {
+      client.set(req.path, list.toString(), function(err) {
+        if (err) {
+          res.statusCode = 500;
+          res.end(err.message);
+          return;
+        }
+        res.statusCode = 204;
+        res.end();
+      })
+    }));
+  }
+});
+
+server.listen(3000);
